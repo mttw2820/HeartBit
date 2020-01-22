d9f9b739989aa31a62db675ba0d70bc959ad65d7
mttw2820 <kooong2820@sookmyung.ac.kr>
2020-01-22 15:54:38 +0900

    search tag and get List names

--- Web_selenium_makeBrowser.py
+++ Web_selenium_makeBrowser.py
@@ -8,7 +8,7 @@
 # melon을 개인 chrome창에 불러오기
 
 from selenium import webdriver
-
+/Users/mttw2820/Desktop/Mac/Github/heartbit/Web_selenium_makeBrowser.py
 # 다운받은 webdriver의 로컬 위치를 path에 저장한다
 path = '/Users/mttw2820/Desktop/Mac/webCrawling/chromedriver'
 driver = webdriver.Chrome(path)
